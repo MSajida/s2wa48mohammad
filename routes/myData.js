722a66c79ce19d0ce366c5acb2267def85eb413a
Sajida <sajida.mohammad2093@gmail.com>
2022-03-29 20:57:46 -0500

    Updated the title

--- routes/myData.js
+++ routes/myData.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('myData', { title: 'Express' });
+  res.render('myData', { title: 'Sajida Mohammad' });
 });
 
 module.exports = router;
